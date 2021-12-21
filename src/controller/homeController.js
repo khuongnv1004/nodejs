f3102091685de8c0c3ef76ceb786c58b00a58df0
Khuong <khuongnv104.hrcneu@gmail.com>
2021-12-21 08:31:56 +0700

    delete and update

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -27,9 +27,27 @@ let getHomepage = async (req, res) => {
 let getDetailPage = async (req, res) => {
     let id = req.params.userID;
     const [user] = await pool.execute(`SELECT * FROM users WHERE id=?`,[id]);
-    
-    return res.send(JSON.stringify(user))
+    return res.render('user',{dataUser: user[0]})
   }
+
+let deleteUser = async (req, res) =>{
+  let userID = req.body.userId;
+  await pool.execute(`DELETE FROM users WHERE id=?`,[userID]);
+  return res.redirect('/')
+
+}
+
+let getEditUser = async (req, res) =>{
+  let userID = req.params.id;
+  let [user] =  await pool.execute(`SELECT * FROM users WHERE id=?`,[userID]);
+  return res.render('editUser', {dataUser: user[0]})}
+
+  let updateUser = async (req, res) => {
+    let {firstName, lastName, email, address, id} = req.body;
+    await pool.execute(`Update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?`,[firstName, lastName, email, address, id]);
+    return res.redirect('/')
+}
+
 let createUser = async (req, res) => {
     let {firstName, lastName, email, address} = req.body;
     await pool.execute(`Insert into users (firstName, lastName, email, address) values (?, ?, ?, ?)`,[firstName, lastName, email, address]);
@@ -38,5 +56,5 @@ let createUser = async (req, res) => {
 }
 
 module.exports = {
-    getHomepage, getDetailPage, createUser
+    getHomepage, getDetailPage, createUser, deleteUser, getEditUser, updateUser
 }
--- src/route/home.js
+++ src/route/home.js
@@ -5,6 +5,9 @@ let router = express.Router();
 const initWebRouter = (app) =>{
     router.get('/', homeController.getHomepage);
     router.get('/detail/user/:userID',homeController.getDetailPage);
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.getEditUser)
+    router.post('/update-user', homeController.updateUser);
     router.post('/create-new-user', homeController.createUser);
 
 
